1a5de7b1c4a8d14452a2e66858c232e6ce3f1125
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-07-17 15:22:41 -0600

    Create IfStatement.java

---
+++ IfStatement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IfStatement{
+
+    public static void main(String[] arg)
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nPlease enter your age: ");
+        int userAge = input.nextInt();
+        if (userAge < 0 || userAge > 100)
+        {
+            System.out.println("Invalid Age");
+            System.out.println("Age must be between 0 and 100");
+        }
+        else if (userAge < 18)
+            System.out.println("Sorry you are underage");
+        else if (userAge < 21)
+            System.out.println("You need parental consent");
+        else
+        {
+            System.out.println("Congratulations!");
+            System.out.println("You may sign up for the event!");
+        }    
+    
+    }    
+}
